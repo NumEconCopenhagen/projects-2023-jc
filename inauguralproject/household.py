af2aaa01dab14b5c68a49e68c46b11e4682649b1
camaansson99 <hjm224@alumni.ku.dk>
2023-03-19 16:33:17 +0100

    Opgave 4 og 5 er skrevet ind

--- inauguralproject/household.py
+++ inauguralproject/household.py
@@ -7,6 +7,8 @@ from scipy import optimize
 import pandas as pd 
 import matplotlib.pyplot as plt
 
+from scipy.optimize import minimize, minimize_scalar
+
 class HouseholdSpecializationModelClass:
 
     def __init__(self):
@@ -139,3 +141,32 @@ class HouseholdSpecializationModelClass:
 
         pass
 
+    def __init__(self, a, w, p, v, wm, wf, sigma_m, sigma_f, beta0, beta1, L_M, W_M, H_M, epsilon):
+        """ setup model """
+        #Defining the float values
+        self.a_values = [0.25, 0.5, 0.75] #These values represent the productivity in home production for females relative to males.
+        self.sigma_values = [0.5, 1.0, 1.5] #These values represent the elasticity of substitution between the home production of males and females in the household.
+        self.H_M_values = np.zeros((len(self.a_values), len(self.sigma_values), 49))
+        self. W_F_values = np.array([0.8, 0.9, 1.0, 1.1, 1.2])
+
+        #Question 5
+        # Define the parameters as instance variables
+        self.a = a
+        self.w = w
+        self.p = p
+        self.v = v
+        self.wm = wm
+        self.wf = wf
+        self.sigma_m = sigma_m
+        self.sigma_f = sigma_f
+        self.beta0 = beta0
+        self.beta1 = beta1
+        self.W_M = W_M
+        self.H_M = H_M
+        self.L_M = L_M
+        self.T_M = L_M+H_M
+        self.epsilon=epsilon
+      
+      
+# Create an instance of the HouseholdSpecializationModel1Class
+model = HouseholdSpecializationModel1Class(a=0.5, w=0.5, p=2, v=0.001, wm=1, wf=1, sigma_m=1.5, sigma_f=2, beta0=0.4, beta1=-0.1, L_M=0, W_M=1, H_M=0, epsilon=1)
